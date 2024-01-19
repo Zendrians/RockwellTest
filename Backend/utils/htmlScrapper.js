5b38dd9f88ad0ecd80403fcae067975b8df48ce4
Daniel Arango Vieira <zendrian@gmail.com>
2024-01-19 16:45:31 -0500

    Refactor scraper logic

--- Backend/utils/htmlScrapper.js
+++ Backend/utils/htmlScrapper.js
@@ -3,7 +3,7 @@ import * as cheerio from "cheerio";
 export function scrapHtml(htmlString) {
   const $ = cheerio.load(htmlString);
   const headers = [];
-  $("pre").each((i, el) => {
+  $("h1, h2, h3, h4, h5, h6").each((i, el) => {
     headers.push({
       tag: el.name,
       text: $(el)
@@ -13,13 +13,18 @@ export function scrapHtml(htmlString) {
     });
   });
   if (headers.length === 0) {
-    const textOnly = $("html *")
+    const textOnly = $("html body *")
       .contents()
-      .map((i, el) => {
-        return el.type === "text" ? `${$(el).text()} ` : "";
+      .filter((i, el) => {
+        return (
+          el.type === "text" &&
+          el.parent.name !== "script" &&
+          el.parent.name !== "noscript" &&
+          el.parent.name !== "style"
+        );
       })
-      .get()
-      .join("");
+      .text();
+
     return {
       type: "text",
       data: textOnly.replace(/\s{3,}/g, " ").substring(0, 999),
