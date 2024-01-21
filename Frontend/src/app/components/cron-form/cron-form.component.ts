e21ac0ce440a4366050fd38bc7080ae1a037b852
Daniel Arango Vieira <zendrian@gmail.com>
2024-01-20 20:41:19 -0500

    Add post req

--- Frontend/src/app/components/cron-form/cron-form.component.ts
+++ Frontend/src/app/components/cron-form/cron-form.component.ts
@@ -7,6 +7,7 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 
+// Move this to utlis
 const urlRegex = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
 var cronRegex = new RegExp(
   /^(\*|([0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9])|\*\/([0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9])) (\*|([0-9]|1[0-9]|2[0-3])|\*\/([0-9]|1[0-9]|2[0-3])) (\*|([1-9]|1[0-9]|2[0-9]|3[0-1])|\*\/([1-9]|1[0-9]|2[0-9]|3[0-1])) (\*|([1-9]|1[0-2])|\*\/([1-9]|1[0-2])) (\*|([0-6])|\*\/([0-6]))$/
@@ -21,6 +22,7 @@ var cronRegex = new RegExp(
 })
 export class CronFormComponent {
   private clientService = inject(ClientService);
+  message: string | null = null;
   scrapFormGroup = new FormGroup({
     webUrl: new FormControl(null, [
       Validators.required,
@@ -32,11 +34,13 @@ export class CronFormComponent {
     ]),
   });
 
-  onSubmit() {
-    this.clientService.logger('X=1')
-    console.log(this.scrapFormGroup.value);
+  async onSubmit() {
     if (this.scrapFormGroup.valid) {
-      console.log('Ready to submit');
+      const res = await this.clientService.postCronScrapJob(
+        this.scrapFormGroup.value.webUrl ?? '',
+        this.scrapFormGroup.value.cronExp ?? ''
+      );
+      this.message = res.message;
     } else {
       console.log('Invalid');
     }
--- Frontend/src/app/services/client.service.ts
+++ Frontend/src/app/services/client.service.ts
@@ -1,10 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom, take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClientService {
+  constructor(private http: HttpClient) {}
+
   logger(data: string) {
     console.log(`You called the service with ${data}`);
   }
+
+  async postCronScrapJob(webUrl: string, cronExp: string) {
+    console.log(webUrl, cronExp);
+    const request = this.http
+      .post<{ message: string }>('http://localhost:5000/api/v1/scrap/cron', {
+        url: webUrl,
+        cronExpression: cronExp,
+      })
+      .pipe(take(1));
+
+    return await lastValueFrom<{ message: string }>(request);
+  }
+
+  postScrap() {}
 }
