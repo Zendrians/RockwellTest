f6c58a0c0a5a2a010577c98fcdc43044b5be9fab
Daniel Arango Vieira <zendrian@gmail.com>
2024-01-20 20:40:40 -0500

    Remove old scrapper code

--- Backend/controllers/scraper.js
+++ Backend/controllers/scraper.js
@@ -1,29 +1,9 @@
 import { CronJob } from "cron";
 import { scrapHtml } from "../utils/htmlScrapper.js";
 
-export const scrapByUrl = async (req, res) => {
-  const { url, cronExpression } = req.body;
-
-  try {
-    const webResponse = await fetch(url);
-    if (webResponse.ok) {
-      const baseHtml = await webResponse.text();
-      const scrapedData = scrapHtml(baseHtml);
-      res.status(200).json({
-        data: scrapedData,
-      });
-    } else {
-      res.status(400).send("Not valid");
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Server Error");
-  }
-};
-
 export const scrapByUrlCron = async (req, res) => {
   const { url, cronExpression } = req.body;
-  if (!url || !cronExpression) throw new Error("params missing");
+  if (!url || !cronExpression) throw new Error("Missing parameters");
 
   console.log(url, cronExpression);
 
@@ -34,7 +14,7 @@ export const scrapByUrlCron = async (req, res) => {
     start: true,
   });
 
-  res.status(202).send("Task successfully scheduled");
+  res.status(202).json({ message: "Task successfully scheduled" });
 };
 
 async function fetchAndScrap(url) {
--- Backend/router/scraperRouter.js
+++ Backend/router/scraperRouter.js
@@ -1,9 +1,8 @@
 import express from "express";
-import { scrapByUrl, scrapByUrlCron } from "../controllers/scraper.js";
+import { scrapByUrlCron } from "../controllers/scraper.js";
 
 const router = express.Router();
 
-router.post("/scrap", scrapByUrl);
 router.post("/scrap/cron", scrapByUrlCron);
 
 export default router;
