5623ac4a5cd3acdfdf2ea15d7ab74764472eb775
Daniel Arango Vieira <zendrian@gmail.com>
2024-01-21 10:42:07 -0500

    Move regex to utils

--- Frontend/src/app/components/cron-form/cron-form.component.ts
+++ Frontend/src/app/components/cron-form/cron-form.component.ts
@@ -6,12 +6,7 @@ import {
   Validators,
   ReactiveFormsModule,
 } from '@angular/forms';
-
-// Move this to utlis
-const urlRegex = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
-var cronRegex = new RegExp(
-  /^(\*|([0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9])|\*\/([0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9])) (\*|([0-9]|1[0-9]|2[0-3])|\*\/([0-9]|1[0-9]|2[0-3])) (\*|([1-9]|1[0-9]|2[0-9]|3[0-1])|\*\/([1-9]|1[0-9]|2[0-9]|3[0-1])) (\*|([1-9]|1[0-2])|\*\/([1-9]|1[0-2])) (\*|([0-6])|\*\/([0-6]))$/
-);
+import { cronRegex, urlRegex } from '../../utils/regex';
 
 @Component({
   selector: 'app-cron-form',
